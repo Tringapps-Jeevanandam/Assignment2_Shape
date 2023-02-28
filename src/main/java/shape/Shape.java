47d9638d8e8db51c8a7c5aa3c6af013a2680c1c9
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-02-28 10:23:53 +0530

    Update Shape.java

--- src/main/java/shape/Shape.java
+++ src/main/java/shape/Shape.java
@@ -13,6 +13,7 @@ class Shape{
         double s1;
         double s2;
         double s3;
+        String temp;
         Scanner sc = new Scanner(System.in);
 
         while(true){
@@ -25,8 +26,10 @@ class Shape{
                     r = sc.nextDouble();
                     shapeName = "Circle";
                     Shape2 sh = new Shape2(shapeName, r);
-                    Log.info("Area of Circle: "+sh.area(r));
-                  Log.info("Perimeter of Circle: "+sh.perimeter(r));
+                    temp = "Area of Circle: "+sh.area(r);
+                    Log.info(temp);
+                    temp = "Perimeter of Circle: "+sh.perimeter(r);
+                  Log.info(temp);
 
                 }break;
                 case 2:{
@@ -35,8 +38,10 @@ class Shape{
                 	wid = sc.nextDouble();
                 	shapeName = "Rectangle";
                 	Shape2 sh = new Shape2(shapeName, len,wid);
-                	Log.info("Area of Rectangle: "+sh.area(len, wid));
-                	Log.info("Perimeter of Rectangle: "+sh.perimeter(len, wid));
+                    temp = "Area of Rectangle: "+sh.area(len, wid);
+                	Log.info(temp);
+                    temp ="Perimeter of Rectangle: "+sh.perimeter(len, wid);
+                	Log.info(temp);
 
                 }break;
                 case 3:{
@@ -46,8 +51,10 @@ class Shape{
                 	s3 = sc.nextDouble();
                 	shapeName = "Triangle";
                 	Shape2 sh = new Shape2(shapeName,s1,s2,s3);
-                	Log.info("Area of Triangle:"+sh.area(s1,s2,s3));
-                	Log.info("Perimeter of Triangle: "+sh.perimeter(s1,s2,s3));
+                    temp ="Area of Triangle:"+sh.area(s1,s2,s3);
+                	Log.info(temp);
+                    temp = "Perimeter of Triangle: "+sh.perimeter(s1,s2,s3);
+                	Log.info(temp);
                 	
                 }break;
                 case 4: System.exit(0); break;
